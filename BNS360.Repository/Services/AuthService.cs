67c41d026aa82a3a2275378ea2ad0a54a2550b13
UOSEF <UOSEF@DESKTOP-SVVTNST>
2024-10-02 19:11:18 +0300

    om

--- BNS360.Repository/Services/AuthService.cs
+++ BNS360.Repository/Services/AuthService.cs
@@ -121,10 +121,10 @@ namespace BNS360.Repository.Services
                 {
                     return new ApiResponse(400, "البريد الإلكتروني أو كلمة المرور غير صحيحة");
                 }
-                //if (!user.EmailConfirmed)
-                //{
-                //    return new ApiResponse(400, "الرجاء تأكيد البريد الإلكتروني الخاص بك");
-                //}
+                if (!user.EmailConfirmed)
+                {
+                    return new ApiResponse(400, "الرجاء تأكيد البريد الإلكتروني الخاص بك");
+                }
                 var jwtToken = await GenerateJwt(user);
                 return new ApiResponse(200, "تم تسجيل الدخول بنجاح", jwtToken);
             }
